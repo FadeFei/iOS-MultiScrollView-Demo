638a2ea2cafac4220392c4fcd30b2c67e28c57f1
GuFei <gufei@bigo.sg>
2018-08-21 18:04:33 +0800

    init the demo

---
+++ MultiScrollViewDemo/MultiScrollViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiScrollViewDemo
+//
+//  Created by gufei on 2018/8/21.
+//  Copyright © 2018年 gufei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
